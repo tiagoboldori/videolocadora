cc83a491c6f56adbba4632e0ef38059f4741dced
Tiago Boldori <158084889+tiagoboldori@users.noreply.github.com>
2025-10-26 22:05:35 -0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  #
+  config.action_cable.mount_path = nil
+  config.action_cable.disable_request_forgery_protection = true
 end
